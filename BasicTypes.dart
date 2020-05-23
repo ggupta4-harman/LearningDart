208ef1055524bd254f1af2b6eb88a2b2a5add00e
Gaurav Gupta <gaurav.gupta42@harman.com>
2020-05-24 00:24:15 +0530

    added file containing basic types

---
+++ BasicTypes.dart
@@ -0,0 +1,9 @@
+  void main(List<String> args) {
+    var name = "Gaurav Gupta"; //String
+    var age = 34;//Int subset of num
+    var skillset = ["iOS","python","dart"]; //list
+    var exploredCountriesAndCities = {"Germany":["Duesseldorf","Frankfurt"]}; //maps
+    
+    print(" my name is: ${name} ");
+    print("I have been to following : $exploredCountriesAndCities");    
+}
